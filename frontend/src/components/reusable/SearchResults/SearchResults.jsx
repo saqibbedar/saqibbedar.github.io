0614a915d773e98e4ea3afa913099cbef33e7573
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-04-08 23:57:41 +0500

    style: customize searchResults component

--- frontend/src/components/reusable/SearchResults/SearchResults.jsx
+++ frontend/src/components/reusable/SearchResults/SearchResults.jsx
@@ -6,19 +6,19 @@ const categoryButtons = ["All", "Projects", "Certificates"]
 const SearchResults = ({ results }) => {
   
   return (
-    <div className="overflow-y-scroll overflow-x-hidden mt-28 w-[90%] media1:w-[96%] m-auto">
-
-      <div className="flex gap-4">
-        {
-          categoryButtons.map((button, index) => (
-
-            <button key={index} className="text-white">{ button }</button>
-          ))
-        }
-      </div>
+    <div className="overflow-y-scroll overflow-x-hidden mt-28 w-[90%] media1:w-[90%] m-auto search-results-hide-scroll-class">
+      {
+        <div className="flex gap-4">
+          {categoryButtons.map((button, index) => (
+            <button key={index} className="text-white">
+              {button}
+            </button>
+          ))}
+        </div>
+      }
 
       {results.certificates.length > 0 && (
-        <div className="flex gap-5 overflow-x-scroll">
+        <div className="flex gap-5 overflow-x-scroll search-results-hide-scroll-class mt-4">
           {results.certificates.map((certificate, index) => (
             <Card
               key={index}
@@ -35,13 +35,34 @@ const SearchResults = ({ results }) => {
 
       {/* Same for projects */}
       {results.projects.length > 0 && (
-        <div className="flex">
+        <div className="flex gap-4 overflow-x-scroll search-results-hide-scroll-class mt-4">
           {results.projects.map((project, index) => (
-            <Link key={index} to={project.url}>
-              <h1>{project.name}</h1>
-              <p>{project.description}</p>
-              <img src={project.image} alt="" />
-              <div>{project.category}</div>
+            <Link
+              key={index}
+              to={project.url}
+              className="flex flex-col gap-2 w-[340px] bg-white overflow-hidden rounded-lg"
+            >
+              <img
+                src={project.image}
+                alt=""
+                className="aspect-video rounded-md"
+              />
+              <div className="px-3">
+                <h1 className="text-[var(--cards-text-title-size)]">
+                  {project.name}
+                </h1>
+                <p className="text-[var(--cards-text-description-foreground)] line-clamp-4">
+                  {project.description}
+                </p>
+                <div className="flex gap-2">
+                  {
+                    // categories are in string, convert them to array and render each category separately
+                    project.category.split(" ").map((category, index) => (
+                      <span key={index}>{category}</span>
+                    ))
+                  }
+                </div>
+              </div>
             </Link>
           ))}
         </div>
