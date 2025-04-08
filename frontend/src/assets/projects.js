44d6d4d37617673fd86817daba94c881f79b8513
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-04-08 23:56:47 +0500

    refactor: add more description to test the project's description length in cards

--- frontend/src/assets/projects.js
+++ frontend/src/assets/projects.js
@@ -3,21 +3,21 @@ const projects = [
         url : "/",
         name: "Insta clone",
         image : "https://imageio.forbes.com/specials-images/imageserve/5f15a9ee916e8500077ad83b/0x0.jpg?format=jpg&width=1200",
-        description: "lorem ipsum dollar amit...",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iusto in quae aspernatur modi, dolorem, incidunt vel odio autem doloremque reprehenderit libero necessitatibus distinctio impedit. Ullam provident similique excepturi quidem!",
         category: "free Nextjs MERN TAILWIND Nodejs Frontend Backend HTML JavaScript React Express MongoDB featured"
     },
     {
         url : "/",
         name: "MERN Portfolio",
         image : "https://www.sotatek.com/wp-content/uploads/2022/12/game-development-services-4.png",
-        description: "lorem ipsum dollar amit...",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iusto in quae aspernatur modi, dolorem, incidunt vel odio autem doloremque reprehenderit libero necessitatibus distinctio impedit. Ullam provident similique excepturi quidem!",
         category: "Premium MERN Nodejs Frontend Backend HTML JavaScript React Express MongoDB featured"
     },
     {
         url : "/",
         name: "Food Ordering App",
         image : "https://www.sotatek.com/wp-content/uploads/2022/12/game-development-services-4.png",
-        description: "lorem ipsum dollar amit...",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iusto in quae aspernatur modi, dolorem, incidunt vel odio autem doloremque reprehenderit libero necessitatibus distinctio impedit. Ullam provident similique excepturi quidem!",
         category: "Premium MERN Nodejs Frontend Backend HTML CSS JavaScript React Express MongoDB"
     },
 ]
