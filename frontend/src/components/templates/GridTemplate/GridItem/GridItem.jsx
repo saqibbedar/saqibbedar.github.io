8cf5a288e0ae9ff0c7823466825993923f6629ba
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-04-11 23:03:53 +0500

    feat: style projects tags

--- frontend/src/components/templates/GridTemplate/GridItem/GridItem.jsx
+++ frontend/src/components/templates/GridTemplate/GridItem/GridItem.jsx
@@ -2,7 +2,7 @@ import "../Grid/Grid.css";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const GridItem = ({ projectUrl, projectName, projectDescription, projectImage, isLoading, setIsLoading }) => {
+const GridItem = ({ projectUrl, projectName, projectDescription, projectImage, tags, isLoading, setIsLoading }) => {
   useEffect(() => {
     setIsLoading(true);
   }, [projectUrl, projectName, projectImage, setIsLoading]);
@@ -18,9 +18,17 @@ const GridItem = ({ projectUrl, projectName, projectDescription, projectImage, i
       style={{ gap: isLoading ? "25px" : "" }}
     >
       <div className="flex flex-col gap-1 projects-title-des-wrapper">
-        <p className={`${ isLoading && "skeleton" } text-xl text-[var(--cards-text-title-foreground)] whitespace-nowrap font-normal project-title`}> • {projectName} </p>
+        <p
+          className={`${
+            isLoading && "skeleton"
+          } text-xl text-[var(--cards-text-title-foreground)] whitespace-nowrap font-normal project-title`}
+        >
+          {" "}
+          • {projectName}{" "}
+        </p>
         <p className="line-clamp-2 tracking-[var(--other-text-letter-spacing)] text-[var(--cards-text-description-foreground)] project-description">
-          {projectDescription || `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ratione
+          {projectDescription ||
+            `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ratione
           fugit expedita rem maiores ad, placeat dolores natus assumenda velit,
           blanditiis nihil voluptates incidunt inventore soluta ipsa ea totam a.`}
         </p>
@@ -33,6 +41,28 @@ const GridItem = ({ projectUrl, projectName, projectDescription, projectImage, i
         }
       >
         <img src={projectImage} onLoad={handleImageLoad} />
+      </div>
+      <div className="flex gap-[6px] flex-wrap text-xs mb-3 leading-3">
+        <span className="text-sm">Tags:</span>
+        {tags.split(" ").map((tag, index) => (
+          <Link
+            to={`/search/${tag}`}
+            key={index}
+            className={`py-1 px-2 rounded-full ${
+              tag.toLowerCase() === "free"
+                ? "bg-green-100 border border-green-600 text-green-600 hover:bg-green-600 hover:text-white"
+                : tag.toLowerCase() === "premium"
+                ? "bg-orange-100 border border-orange-400 text-orange-400 hover:bg-orange-400 hover:text-white"
+                : tag.toLowerCase() === "featured"
+                ? "bg-[#2563eb]/10 border border-[#2563eb] text-[#2563eb] hover:bg-[#2563eb] hover:text-white"
+                : tag.toLowerCase() === "private"
+                ? "bg-slate-100 border border-slate-600 text-slate-600 hover:bg-slate-600 hover:text-white"
+                : "bg-violet-100 border border-violet-600 text-violet-600 hover:bg-violet-600 hover:text-white"
+            }`}
+          >
+            {tag.toUpperCase()}
+          </Link>
+        ))}
       </div>
     </Link>
   );
