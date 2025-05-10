efe615446e1d680dc261259c3027e541836b5587
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-05-10 23:49:31 +0500

    refactor: utilize useProjects() hook for projects data

--- frontend/src/components/layouts/AboutLayout/AboutProjectsSection/AboutProjectSection.jsx
+++ frontend/src/components/layouts/AboutLayout/AboutProjectsSection/AboutProjectSection.jsx
@@ -2,21 +2,21 @@ import "./AboutProjectSection.css";
 import { useContext, useState } from "react";
 import { GridContext } from "@/context/GridContext";
 import { HeroBtn } from "@/components/reusable/reusable";
-import { ProjectContext } from "@/context/ProjectContext";
+import { useProjects } from "@/context/context";
 import { Grid, GridToggler, GridItem } from "@/components/templates/templates";
 
 const AboutProjectSection = () => {
   const { isGrid } = useContext(GridContext);
 
-  const { featuredProjects } = useContext(ProjectContext);
-
+  const { getProjects } = useProjects();
+  const projects = getProjects("featured");
   const [isLoading, setIsLoading] = useState(true);
 
   return (
     <div className="about-sec-project-wrapper">
       <GridToggler section_name={"Top Projects"} />
       <Grid isGrid={isGrid} gridTempCol={"1fr 1fr 1fr"}>
-        {featuredProjects.map((project, index) => (
+        {projects.map((project, index) => (
           <GridItem
             key={index}
             projectUrl={project.url}
