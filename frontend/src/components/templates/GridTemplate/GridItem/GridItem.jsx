ab5874f3a4ec65e5f821a857aa19cd0ae17321f1
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-07-12 06:46:51 +0500

    refactor: update Grid template - still design is not complete but mostly design match with new theme design

--- frontend/src/components/templates/GridTemplate/GridItem/GridItem.jsx
+++ frontend/src/components/templates/GridTemplate/GridItem/GridItem.jsx
@@ -1,6 +1,6 @@
 import "../Grid/Grid.css";
-import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import SimpleParallax from "simple-parallax-js"
 
 const GridItem = ({ projectId=null, projectUrl, projectName, projectDescription, projectImage, projectTags, isLoading=null, setIsLoading=null }) => {
 
@@ -10,32 +10,39 @@ const GridItem = ({ projectId=null, projectUrl, projectName, projectDescription,
     <Link to={`/projects/${projectId}`} className="grid-item shadow-sm">
       <div className="flex flex-col gap-1 projects-title-des-wrapper">
         <p
-          className={`text-xl text-[var(--cards-text-title-foreground)] whitespace-nowrap font-normal project-title`}
+          className={`text-xl text-[var(--dt-title-text)] whitespace-nowrap font-semibold project-title`}
         >
-          {" "}
-          • {projectName}{" "}
+          {` • ${projectName} `}
         </p>
-        <p className="line-clamp-2 tracking-[var(--other-text-letter-spacing)] text-[var(--cards-text-description-foreground)] project-description">
+        <p className="line-clamp-2 tracking-[var(--other-text-letter-spacing)] text-[var(--dt-subtitle-text)] project-description">
           {projectDescription ||
             `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ratione
           fugit expedita rem maiores ad, placeat dolores natus assumenda velit,
           blanditiis nihil voluptates incidunt inventore soluta ipsa ea totam a.`}
         </p>
       </div>
-      <div
-        className={"grid-item-img-container"}
-      >
-        <img src={projectImage} />
+
+      <div className={"grid-item-img-container"}>
+        <SimpleParallax
+          scale={1.1}
+        >
+          <img src={projectImage} />
+        </SimpleParallax>
       </div>
+
       <div className="flex gap-[6px] flex-wrap text-xs mb-3 leading-3">
-        <span className="text-sm">Tags:</span>
+        <span className="text-sm text-[var(--dt-title-text)] font-semibold">
+          Tags:
+        </span>
         {projectTags?.split(" ").map((tag, index) => (
           <div
             key={index}
             onClick={(e) => {
               e.stopPropagation();
               e.preventDefault();
-              navigate(`/search?q=${encodeURIComponent(tag.trim().toLowerCase())}`);
+              navigate(
+                `/search?q=${encodeURIComponent(tag.trim().toLowerCase())}`
+              );
             }}
             className={`py-1 px-2 rounded-full ${
               tag.toLowerCase() === "free"
