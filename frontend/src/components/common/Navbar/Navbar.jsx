f7541d9f53160c1a2b0b63637d7a3bf156d9f4f7
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-07-12 06:44:51 +0500

    refactor: update Navabar component - remove previous styles - clean up component - modern design inpired from OpenAI & Apples website

--- frontend/src/components/common/Navbar/Navbar.jsx
+++ frontend/src/components/common/Navbar/Navbar.jsx
@@ -1,137 +1,72 @@
 import "./Navbar.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { author, icons } from "@/assets/assets";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-
-  const location = useLocation(); // for paths 
-  const [menu, setMenu] = useState(location.pathname); 
+  const location = useLocation();
   const [activeMenu, setActiveMenu] = useState(false);
-
-  const navigate = useNavigate()
-
-  // for handling navbar
-  const [isHidden, setIsHidden] = useState(false);
-  const [lastScrollTop, setLastScrollTop] = useState(0);
-
-  // Logic: Navbar open and close based on scrolling
-  useEffect(() => {
-    function handleScroll() {
-      const currentScroll =
-        window.pageYOffset || document.documentElement.scrollTop;
-
-      // Scrolling down, hide the navbar
-      if (currentScroll > lastScrollTop) {
-        setIsHidden(true);
-      } else {
-        // Scrolling up, show the navbar
-        setIsHidden(false);
-      }
-
-      setLastScrollTop(currentScroll <= 0 ? 0 : currentScroll);
-    }
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [lastScrollTop]);
-
-  const handleMenu = () => {
-    setActiveMenu(!activeMenu);
-    if (!activeMenu) {
-      document
-        .querySelector(".mobile-nav-menu")
-        .classList.remove("active-mobile-menu");
-
-      document.querySelectorAll(".mb-nav-link").forEach((elem) => {
-        elem.addEventListener("click", () => {
-          setActiveMenu(activeMenu);
-        });
-      });
-    }
-  };
+  const navigate = useNavigate();
 
   return (
-    <>
-      <div className={`w-full fixed navbar px-[26px] media1:px-[80px] mediaXl:px-8 mediaXXl:px-20 ${isHidden ? "hidden" : ""}`}>
-        {/* DESKTOP LOGO */}
+    <header className="navbar-wrapper">
+
+      {/* DESKTOP */}
+      <div className="navbar">
         <div className="logo">
-          <h1 translate="no">
-            <Link to={"/"}>{author.logo + "."}</Link>
+          <h1>
+            <Link to="/">{author.logo + "."} </Link>
           </h1>
         </div>
-        {/* NAVBAR ELEMENTS */}
         <nav className="nav-menu">
-          {["home", "about", "blogs", "projects", "services", "courses", "contact"].map((element, index) => {
-            const elementPath = `/${element}`;
-            const isActive =
-              location.pathname === elementPath || location.pathname.includes(element) ||
-              (location.pathname === "/" && elementPath === "/home");
-            return (
-              <Link
-                to={elementPath}
-                key={index}
-                onClick={() => {
-                  setMenu(elementPath);
-                }}
-                className={isActive ? "active-nav-des-menu" : "none"}
-                style={{ color: isActive ? "white" : "" }}
-              >
-                {element}
-              </Link>
-            );
-          })}
-          {/* DESKTOP SEARCH */}
-          <Link to={"/Search"} className="search search-large-screen">
-            <icons.search />
-          </Link>
+          {["home", "about", "projects", "services", "courses", "contact"].map(
+            (item) => {
+              const path = `/${item}`;
+              const isActive =
+                location.pathname === path ||
+                (location.pathname === "/" && path === "/home");
+
+              return (
+                <Link key={item} to={path} className={isActive ? "active" : ""}>
+                  {item}
+                </Link>
+              );
+            }
+          )}
         </nav>
         
-        {/* HAMBURGER FOR MOBILE */}
-        <div className="icons">
-          <div className="hamburger" onClick={handleMenu}>
-            <icons.hamburger />
-          </div>
+        {/*  */}
+
+        <div className="navbar-tools">
+          <button onClick={()=>navigate('/search')}>
+            <icons.search />
+          </button>
         </div>
+
+        {/* Mobile Menu Toggle */}
+        <button className="hamburger" onClick={()=>setActiveMenu(!activeMenu)} aria-label="Toggle menu">
+          { activeMenu ? <icons.close /> : 
+          <svg width="18" height="18" viewBox="0 0 18 18"><polyline id="globalnav-menutrigger-bread-bottom" fill="none" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" points="2 12, 16 12" class="globalnav-menutrigger-bread globalnav-menutrigger-bread-bottom"><animate id="globalnav-anim-menutrigger-bread-bottom-open" attributeName="points" keyTimes="0;0.5;1" dur="0.24s" begin="indefinite" fill="freeze" calcMode="spline" keySplines="0.42, 0, 1, 1;0, 0, 0.58, 1" values=" 2 12, 16 12; 2 9, 16 9; 3.5 15, 15 3.5"></animate><animate id="globalnav-anim-menutrigger-bread-bottom-close" attributeName="points" keyTimes="0;0.5;1" dur="0.24s" begin="indefinite" fill="freeze" calcMode="spline" keySplines="0.42, 0, 1, 1;0, 0, 0.58, 1" values=" 3.5 15, 15 3.5; 2 9, 16 9; 2 12, 16 12"></animate></polyline><polyline id="globalnav-menutrigger-bread-top" fill="none" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" points="2 5, 16 5" class="globalnav-menutrigger-bread globalnav-menutrigger-bread-top"><animate id="globalnav-anim-menutrigger-bread-top-open" attributeName="points" keyTimes="0;0.5;1" dur="0.24s" begin="indefinite" fill="freeze" calcMode="spline" keySplines="0.42, 0, 1, 1;0, 0, 0.58, 1" values=" 2 5, 16 5; 2 9, 16 9; 3.5 3.5, 15 15"></animate><animate id="globalnav-anim-menutrigger-bread-top-close" attributeName="points" keyTimes="0;0.5;1" dur="0.24s" begin="indefinite" fill="freeze" calcMode="spline" keySplines="0.42, 0, 1, 1;0, 0, 0.58, 1" values=" 3.5 3.5, 15 15; 2 9, 16 9; 2 5, 16 5"></animate></polyline></svg>
+          }
+        </button>
       </div>
 
-      <nav
-        className={`mobile-nav-menu ${activeMenu ? "active-mobile-menu" : ""} ${
-          isHidden ? "hidden" : ""
-        }`}
-      >
-        <div className="search-bar-small-screens">
-          <div onClick={()=> navigate("/search")} className="search search-small-screens">
-            <icons.search /> <span>Search</span>
-          </div>
-        </div>
-
-        {["home", "about", "blogs", "projects", "services", "courses", "contact"].map((element, index) => {
-          return (
-            <Link
-              to={`/${element}`}
-              key={index}
-              id={element === "home" && "mob-home"}
-              className={`mb-nav-link 
-                ${
-                index === 6 &&
-                "mobile-menu-last-element"
-                }
-              `}
-            >
-              {element.toUpperCase()}
-            </Link>
-          );
-        })}
-        <div className="close-nav" onClick={handleMenu}>
-          <icons.close title="close" />
-        </div>
+      {/* Mobile Navigation */}
+      <nav className={`mobile-nav ${activeMenu ? "active" : ""}`}>
+        {[
+          "home",
+          "about",
+          "projects",
+          "services",
+          "courses",
+          "contact"
+        ].map((item) => (
+          <Link key={item} to={`/${item}`} onClick={()=>setActiveMenu(!activeMenu)}>
+            {item}
+          </Link>
+        ))}
       </nav>
-
-    </>
+    </header>
   );
 };
 
