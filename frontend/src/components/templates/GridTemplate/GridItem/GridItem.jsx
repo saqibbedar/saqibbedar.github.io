263e24db6a38a0fd0f64d04ad10ec9d34e18461c
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-08-08 07:17:18 +0500

    update Grid template

--- frontend/src/components/templates/GridTemplate/GridItem/GridItem.jsx
+++ frontend/src/components/templates/GridTemplate/GridItem/GridItem.jsx
@@ -9,12 +9,12 @@ const GridItem = ({ projectId=null, projectUrl, projectName, projectDescription,
   return (
     <Link to={`/projects/${projectId}`} className="grid-item shadow-sm">
       <div className="flex flex-col gap-1 projects-title-des-wrapper">
-        <p
-          className={`text-xl text-[var(--dt-title-text)] whitespace-nowrap font-semibold project-title`}
+        <h1
+          className={`text-xl whitespace-nowrap font-semibold project-title`}
         >
-          {` • ${projectName} `}
-        </p>
-        <p className="line-clamp-2 tracking-[var(--other-text-letter-spacing)] text-[var(--dt-subtitle-text)] project-description">
+          {`${projectName} `}
+        </h1>
+        <p className="line-clamp-2 mt-2 project-description">
           {projectDescription ||
             `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ratione
           fugit expedita rem maiores ad, placeat dolores natus assumenda velit,
@@ -23,17 +23,12 @@ const GridItem = ({ projectId=null, projectUrl, projectName, projectDescription,
       </div>
 
       <div className={"grid-item-img-container"}>
-        <SimpleParallax
-          scale={1.1}
-        >
+        <SimpleParallax scale={1.1}>
           <img src={projectImage} />
         </SimpleParallax>
       </div>
 
       <div className="flex gap-[6px] flex-wrap text-xs mb-3 leading-3">
-        <span className="text-sm text-[var(--dt-title-text)] font-semibold">
-          Tags:
-        </span>
         {projectTags?.split(" ").map((tag, index) => (
           <div
             key={index}
@@ -43,18 +38,7 @@ const GridItem = ({ projectId=null, projectUrl, projectName, projectDescription,
               navigate(
                 `/search?q=${encodeURIComponent(tag.trim().toLowerCase())}`
               );
-            }}
-            className={`py-1 px-2 rounded-full ${
-              tag.toLowerCase() === "free"
-                ? "bg-green-100 border border-green-600 text-green-600 hover:bg-green-600 hover:text-white"
-                : tag.toLowerCase() === "premium"
-                ? "bg-orange-100 border border-orange-400 text-orange-400 hover:bg-orange-400 hover:text-white"
-                : tag.toLowerCase() === "featured"
-                ? "bg-[#2563eb]/10 border border-[#2563eb] text-[#2563eb] hover:bg-[#2563eb] hover:text-white"
-                : tag.toLowerCase() === "private"
-                ? "bg-slate-100 border border-slate-600 text-slate-600 hover:bg-slate-600 hover:text-white"
-                : "bg-violet-100 border border-violet-600 text-violet-600 hover:bg-violet-600 hover:text-white"
-            }`}
+            }} className={`py-1 px-2 rounded-full text-[var(--dt-trinary-foreground)] hover:text-[var(--dt-primary-foreground)] bg-transparent hover:bg-[var(--dt-card-button-hover-background)] border border-[var(--dt-border-color)] hover:border-[var(--dt-card-button-hover-background)] text-xs transition-[var(--transition)] `}
           >
             {tag.toUpperCase()}
           </div>
