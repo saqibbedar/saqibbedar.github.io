f56d3e858b07a97ace67f8d349a10f366b3817a2
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-08-08 07:14:51 +0500

    update cover page

--- frontend/src/components/sections/home/CoverSection/CoverSection.jsx
+++ frontend/src/components/sections/home/CoverSection/CoverSection.jsx
@@ -1,27 +1,51 @@
-import "./CoverSection.css"
-import { Link } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import "./CoverSection.css";
+import { Button } from "@/components/ui";
+
 // csw: cover section wrapper
 const CoverSection = () => {
+
+  const videoRef = useRef();
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.7;
+    }
+  }, []);
+
   return (
-    <div className='csw'>
-      <button className="available-button">Available</button>
-      <div className="tagline">
-        <h1>Think Bold.</h1>
-        <h1>Build Bedar.</h1>
-      </div>
-      <div className="video-container">
-        <video src="./src/assets/videos/vlogIntro.mp4" autoPlay loop muted></video>
-      </div>
-      <div className="action-buttons">
-        <button>
-          <Link to={"/about"}>Sign me a Project</Link>
-        </button>
-        <button>
-          <Link to={"/about"}>View Portfolio</Link>
-        </button>
+    <div className="cover-section-wrapper">
+      <div className="cover-section-content-overlay">
+        <div className="cover-section-content">
+          <Button
+            variant="navigation"
+            label="Available"
+            to="/services"
+            bg="var(--dt-btn-pri-bg)"
+            hoverBg="var(--dt-btn-pri-hover-bg)"
+          />
+          <div className="tagline">
+            <span>Think</span>
+            <span>Bold.</span>
+            <span>Build</span>
+            <span>Bedar.</span>
+          </div>
+          <div className="action-buttons">
+            <Button
+              variant="navigation"
+              label="Sign a Project"
+              to="/"
+              fg="var(--dt-btn-sec-fg)"
+              bg="var(--dt-btn-sec-bg)"
+              hoverFg="var(--dt-btn-sec-fg"
+              hoverBg="var(--dt-btn-sec-hover-bg)"
+            />
+            <Button variant="navigation" label="View Portfolio" to="/about" />
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CoverSection
+export default CoverSection;
