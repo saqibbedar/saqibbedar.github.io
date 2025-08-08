c843a3f8ecd330fa62a6b28e1e90267f0ee6a7c6
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-08-08 07:06:13 +0500

    fix: update easing value for smooth scroll

--- frontend/src/hooks/useSmoothScroll.js
+++ frontend/src/hooks/useSmoothScroll.js
@@ -5,7 +5,7 @@ export const useSmoothScroll = () => {
     useEffect(() => {
         const lenis = new Lenis({
           duration: 1,
-          easing: (t) => 1 - Math.pow(1 - t, 5), // Use smoother easing
+          easing: (t) => 1 - Math.pow(1 - t, 2), // Use smoother easing
           smoothWheel: true,
           wheelMultiplier: 1,
           smoothTouch: false,
