68cbad03801dc2841068ad30ba16d693be05438c
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-08-08 07:05:06 +0500

    feat: add react-fast-marquee pkg for skills marquee and update UI

--- frontend/src/components/sections/home/SkillSection/SkillSection.jsx
+++ frontend/src/components/sections/home/SkillSection/SkillSection.jsx
@@ -5,28 +5,34 @@ import {Link} from "react-router-dom"
 
 const SkillSection = () => {
   return (
-    <div className="skill-wrapper">
-      <Marquee pauseOnHover={true} autoFill={true} speed={25}>
-          <div className="marquee-row">
-            {skills.first_row.map((skill, index) => (
-              <Link to={`/search?q=${skill.name}`} key={index} className="skill-box">
-                {<skill.icon />}
-                <h1>{skill.name}</h1>
-              </Link>
-            ))}
-          </div>
-      </Marquee>
+    <div className="skill-wrapper w-[90%] m-auto rounded-md p-10">
+      <div className="">
+        <h2 className="text-[48px] font-medium text-center">Tools of Trade </h2>
+      </div>
+      <div className="marquee-wrapper">
+        <Marquee pauseOnHover={true} autoFill={true} speed={25}>
+            <div className="marquee-row">
+              {skills.first_row.map((skill, index) => (
+                <Link to={`/search?q=${skill.name}`} key={index} className="skill-box">
+                  {<skill.icon />}
+                  <h1>{skill.name}</h1>
+                </Link>
+              ))}
+            </div>
+        </Marquee>
 
-      <Marquee pauseOnHover={true} autoFill={true} speed={25} direction="right">
-          <div className="marquee-row">
-            {skills.second_row.map((skill, index) => (
-              <Link to={`/search?q=${skill.name}`} key={index} className="skill-box">
-                {<skill.icon />}
-                <h1>{skill.name}</h1>
-              </Link>
-            ))}
-          </div>
-      </Marquee>
+        <Marquee pauseOnHover={true} autoFill={true} speed={25} direction="right">
+            <div className="marquee-row">
+              {skills.second_row.map((skill, index) => (
+                <Link to={`/search?q=${skill.name}`} key={index} className="skill-box">
+                  {<skill.icon />}
+                  <h1>{skill.name}</h1>
+                </Link>
+              ))}
+            </div>
+        </Marquee>
+      </div>
+
     </div>
   );
 };
