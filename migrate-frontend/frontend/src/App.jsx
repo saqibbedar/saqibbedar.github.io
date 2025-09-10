09ce48131af2687e44815cd2252ec229f719229f
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-09-10 14:21:34 +0500

    chore(migrate): migrating complete to latest versions

---
+++ migrate-frontend/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return ( 
+    <div className="selection:text-black selection:bg-white">
+      Hello, world!
+    </div>
+  );
+}
+
+export default App;
