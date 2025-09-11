7afefaeb243d861aec89d7dc31e5c2bbc097f1c2
saqibbedar <Chandio.Saqib.M@gmail.com>
2025-09-11 23:19:18 +0500

    Style(ProjectSectionComp): redesign home:ProjectSectionComponent & update project tags

--- frontend/src/components/sections/home/ProjectSection/ProjectSection.jsx
+++ frontend/src/components/sections/home/ProjectSection/ProjectSection.jsx
@@ -1,14 +1,8 @@
-import { useContext } from "react";
-import { Button } from "@/components/ui";
-import { Grid, GridToggler, GridItem } from "@/components/templates/templates";
-import { useProjects, GridContext } from "@/context";
+import { useProjects } from "@/context";
 
 const ProjectSection = () => {
   
-  const {isGrid} = useContext(GridContext);
-  
   const { getProjects, loading, error } = useProjects();
-
   const featuredProjects = getProjects("featured");
 
   if (error) {
@@ -16,30 +10,20 @@ const ProjectSection = () => {
   }
 
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-center text-5xl my-14">
-        <h1 >Projects that bring things differently</h1>
-      </div>
-      <Grid isGrid={isGrid} gridTempCol={"1fr 1fr"}>
-        {featuredProjects.map((project, index) => (
-          <GridItem
-            key={index}
-            projectId={project._id}
-            projectUrl={project.url}
-            projectName={project.name}
-            projectImage={project.image}
-            projectTags={project.tags}
-          />
-        ))}
-      </Grid>
-
-      <div className="hero-btn-wrapper w-full flex justify-center mt-[2rem] media1:mt-[2.5rem]">
-        <Button
-          variant="navigation"
-          to="/projects"
-          label="View Projects"
-          size="large"
-        />
+    <div>
+    <div className="my-20 mx-8 auto">
+      {featuredProjects.map((project, index) => (
+        <div
+        key={index}
+        className={`w-full flex items-center justify-between border-white/10 ${index === 0 ? "border-b-[.1px] border-t-[.1px]" : "border-b-[.1px]"}`}
+        >
+          <div className="py-8 lg:text-[6vw] font-semibold uppercase text-[var(--dt-sec-fg)]">
+          {project.name}
+          </div>
+          <div>2020</div>
+        </div>
+      ))}
+      <button>View More</button>
       </div>
     </div>
   );
