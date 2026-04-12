c6627b054b55e6ddacfba618ca33d8b08f834136
saqibbedar <chandio.saqib.m@gmail.com>
2026-04-12 22:27:52 +0500

    updated cors origin

--- backend/index.js
+++ backend/index.js
@@ -8,17 +8,9 @@ const app = express();
 // middlewares
 app.use(express.json());
 
-// Allowed origins
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-  "https://saqibbedar.github.io",
-].filter(Boolean);
-
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
   })
 );
